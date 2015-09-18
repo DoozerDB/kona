44b79ee7b2f48dde55c94824bc6610418c1e0e60
DoozerDB <doozer.db@yandex.ru>
2015-09-18 11:07:55 +0300

    svg ON

--- functions.php
+++ functions.php
@@ -150,3 +150,10 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+/** Enable svg load */
+function cc_mime_types($mimes) {
+  $mimes['svg'] = 'image/svg+xml';
+  return $mimes;
+}
+add_filter('upload_mimes', 'cc_mime_types');
