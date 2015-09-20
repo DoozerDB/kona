0f447b5d9edae96f808f1bd72dfc11603713fd7e
Владимир <doozer.db@yandex.ru>
2015-09-20 22:00:16 +0300

    some changes

--- starter.php
+++ starter.php
@@ -5,12 +5,13 @@ Template Name: Starter
 ?>
 <?php get_header(); ?>
 
-	<div class="mainpicture">
+	<div class="main_image_div">
 		<?php if ( get_theme_mod('main_image') ) : ?>
 			<img class="main_image" src="<?php echo esc_url(get_theme_mod('main_image')); ?>" alt="" />
 		<?php else : ?>
+			<img class="main_image" src="img/mainpic.svg" alt="" />
 		<?php endif; ?>
-	</div>
+	</div><!-- .main_image_div -->
 	<main id="main" class="site-main" role="main">
     <?php the_post()?>
 		<?php get_template_part( 'template-parts/content', 'page' ); ?>
